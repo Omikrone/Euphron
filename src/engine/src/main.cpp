03d75265d1fe6dd20f9f1be2659d76befb7816cd
Roman NITZSCHE <roman.nitzsche67@gmail.com>
2025-09-29 21:53:16 +0200

    First commit -> Initialisation of Euphron

---
+++ src/engine/src/main.cpp
@@ -0,0 +1,13 @@
+// main.cpp
+
+#include "game.hpp"
+
+#include <iostream>
+#include <vector>
+#include <iostream>
+
+int main() {
+
+    std::cout << "Hello world";
+    std::vector<std::unique_ptr<Game>> games;
+}

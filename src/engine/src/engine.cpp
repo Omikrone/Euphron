a5310942f6767d33e9aa573cc90e1707b78561b4
Roman NITZSCHE <roman.nitzsche67@gmail.com>
2025-10-03 18:03:28 +0200

    Fix current turn issue and illegal moves -> successful random game

--- src/engine/src/engine.cpp
+++ src/engine/src/engine.cpp
@@ -6,14 +6,26 @@
 Engine::Engine() : _game(), _infos() {}
 
 void Engine::update_position(std::string fen, BBMove bb_move) {
-    _game.try_apply_move(bb_move.from, bb_move.to);
+    std::cout << "before update : " + _game.get_fen() << std::endl;
+    bool res = _game.try_apply_move(bb_move.from, bb_move.to);
+    if (!res) std::cout << "ILLEGAL PSOITION" << std::endl;
     _game.next_turn();
+    std::cout << "after update : " + _game.get_fen() << std::endl;
 }
 
 
 BBMove Engine::find_best_move() {
+    std::cout << "BEFORE move : " + _game.get_fen() << std::endl;
     std::vector<Move> moves = _game.getAllMoves(_game.get_current_turn());
+    std::cout << moves.size() << std::endl;
+    std::srand(std::time({}));
     int random_i = rand() % moves.size();
     Move random_move = moves.at(random_i);
+    std::cout << random_move.from << " " << random_move.to << std::endl;
+    std::cout << "Current turn : " << _game.get_current_turn() << std::endl;
+    bool res = _game.try_apply_move(random_move.from, random_move.to);
+    if (!res) std::cout << "ILLEGAL MOVE" << std::endl;
+    _game.next_turn();
+    std::cout << "After move : " + _game.get_fen() << std::endl;
     return {random_move.from, random_move.to};
 }

15a4fb476a15add529b67ebfc6912fd6f3584374
Roman NITZSCHE <roman.nitzsche67@gmail.com>
2025-10-07 23:01:11 +0200

    Added piece values for material evaluation of the game

---
+++ src/engine/src/evaluation/piece_values.hpp
@@ -0,0 +1,11 @@
+// piece_values.hpp
+
+
+constexpr int PIECE_VALUES[6] = {
+    100,    // Pawn
+    320,    // Knight
+    340,    // Bishop
+    500,    // Rook
+    900,    // Queen
+    20000   // King
+};

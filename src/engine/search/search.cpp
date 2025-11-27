37075398c60b580b94311c263965e28071a8ef2d
NITZSCHE ROMAN <roman.nitzsche@etu.unistra.fr>
2025-11-27 22:46:49 +0100

    Implemented iterative deepening for minimax

--- src/engine/search/search.cpp
+++ src/engine/search/search.cpp
@@ -16,15 +16,17 @@ int Search::node(int current_depth, int max_depth, bool &search_flag, Color maxi
 
     if (current_depth == max_depth) {        
         GameState state = _game.get_game_state();
-        _game.next_turn();
+        int score;
         if (state == GameState::DRAW_BY_75_MOVE_RULE || 
             state == GameState::STALEMATE || 
             state == GameState::DRAW_BY_INSUFFICIENT_MATERIAL || 
             state == GameState::DRAW_BY_FIFTY_MOVE_RULE ||
-            state == GameState::DRAW_BY_THREEFOLD_REPETITION) return 0;
-        else if (state == GameState::CHECKMATE && current_turn == maximizing_player) return -1000000;
-        else if (state == GameState::CHECKMATE) return 1000000;
-        else return Evaluation::evaluate_board_for(_game.get_board(), maximizing_player);
+            state == GameState::DRAW_BY_THREEFOLD_REPETITION) score = 0;
+        else if (state == GameState::CHECKMATE && current_turn == maximizing_player) score = -1000000;
+        else if (state == GameState::CHECKMATE) score = 1000000;
+        else score = Evaluation::evaluate_board_for(_game.get_board(), maximizing_player);
+        _game.next_turn();
+        return score;
     }
 
     int score;
@@ -53,42 +55,56 @@ int Search::node(int current_depth, int max_depth, bool &search_flag, Color maxi
 }
 
 
-void Search::minimax(std::optional<int> time_limit, int depth, std::vector<Move> &best_moves, bool &search_flag) {
-    std::cout << "beginning minimax" << std::endl;
-    int best_score = -200000;
-    std::cout << "Value of options :" << std::endl;
-    std::cout << "  - time limit : " << (time_limit.has_value() ? std::to_string(time_limit.value()) : "none") << std::endl;
-    std::cout << "  - depth : " << depth << std::endl;
+void Search::minimax(std::optional<int> time_limit, int max_depth, std::vector<Move> &best_moves, bool &search_flag) {
 
+    int depth = 1;
+    int best_score = -200000;
+    
     Color current_turn = _game.get_current_turn();
     std::vector<Move> moves = _game.get_legal_moves(current_turn);
-    for (Move& m : moves) {
-        std::cout << "Flag status : " << search_flag << std::endl;
-        if (!search_flag) break;
-        bool res = _game.try_apply_move(m.from, m.to);
-        if (!res) {
-            std::cout << "Invalid move : " << std::endl;
-        }
-        
-        int score = node(0, depth - 1, search_flag, current_turn);
+    std::cout << "beginning minimax" << std::endl;
+    std::cout << "Value of options :" << std::endl;
+    std::cout << "  - time limit : " << (time_limit.has_value() ? std::to_string(time_limit.value()) : "none") << std::endl;
+    std::cout << "  - depth : " << max_depth << std::endl;
 
-        if (score == best_score) {
-            best_moves.push_back(m);
+    while (depth <= max_depth)
+    {
+        if (!search_flag) break;
+        best_score = -200000; // Extremum to update
+        std::vector<Move> current_depth_best_moves;
+
+        std::cout << "Searching at depth " << depth << std::endl;
+        for (Move& m : moves) {
+            if (!search_flag) break;
+            bool res = _game.try_apply_move(m.from, m.to);
+            if (!res) {
+                std::cout << "Invalid move : " << std::endl;
+            }
+            
+            int score = node(1, depth, search_flag, current_turn);
+
+            if (score == best_score) {
+                std::cout << "Found another best move with score " << score << " Move: " << m.to_uci() << std::endl;
+                current_depth_best_moves.push_back(m);
+            }
+            else if (score > best_score) {
+                std::cout << "Found new best move with score " << score << " Move: " << m.to_uci() << std::endl;
+                best_score = score;
+                current_depth_best_moves.clear(); // TODO : clear only move with inferior score
+                current_depth_best_moves.push_back(m);
+            }
+            _game.unmake_move();
         }
-        else if (score > best_score) {
-            best_score = score;
-            best_moves.clear(); // TODO : clear only move with inferior score
-            best_moves.push_back(m);
-        }
-        _game.unmake_move();
+        if (search_flag) best_moves = current_depth_best_moves;
+        depth++;
     }
-    int current_score = Evaluation::evaluate_board_for(_game.get_board(), current_turn);
-    std::cout << "Final score relative to current position : " << best_score - current_score << std::endl;
-    std::cout << "End of minimax" << std::endl;
     if (best_moves.size() == 0) {
+        std::cout << "No best moves found, defaulting to all legal moves." << std::endl;
         for (Move& m : moves) {
             best_moves.push_back(m);
         }
     }
+    std::cout << "Best score found: " << best_score << std::endl;
+    std::cout << "End of minimax" << std::endl;
     std::cout << "Number of best moves found: " << best_moves.size() << std::endl;
 }

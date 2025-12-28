462c6be90053b66f233f973200585d606d97b4cc
Roman Nitzsche <roman.nitzsche67@gmail.com>
2025-12-28 19:03:38 +0100

    feat(check): add check moves to quiescence search

--- src/engine/search/quiescence.cpp
+++ src/engine/search/quiescence.cpp
@@ -19,28 +19,26 @@ int Quiescence::quiescence(int q_depth, Color maximizing_player, int alpha, int 
             stand_pat = 0;
         else if (state == GameState::CHECKMATE && current_turn == maximizing_player) 
             stand_pat = -1000000;
-        else if (state == GameState::CHECKMATE) 
+        else if (state == GameState::CHECKMATE)
             stand_pat = 1000000;
         return stand_pat;
     }
     else {
-        stand_pat = Evaluation::evaluate_board_for(_game.get_board(), maximizing_player);
+        stand_pat = Evaluation::evaluate_board_for(_game.get_board(), _game.get_current_turn());
         if (q_depth >= MAX_Q_DEPTH) return stand_pat;
         if (stand_pat >= beta) {
             return beta;
         }
         if (stand_pat > alpha) alpha = stand_pat;
 
-        std::vector<Move> moves = _game.get_capture_moves();
+        std::vector<Move> moves = _game.get_capture_and_check_moves();
         _mvv_lva.sort_mvv_lva(moves, _game.get_current_turn());
 
-        //std::cout <<"Number of capturing moves to explore : " << moves.size() << std::endl;
         for (Move m: moves) {
             if (!search_flag) break;
             Bitboards& board = _game.get_board();
             PieceType capturer = board.get_piece_type(current_turn, m.from);
             PieceType captured = board.get_piece_type(opponent, m.to);
-            if (PIECE_VALUES[capturer] > PIECE_VALUES[captured]) continue;
 
             bool res = _game.try_apply_move(m.from, m.to);
             if (!res) {
@@ -49,7 +47,7 @@ int Quiescence::quiescence(int q_depth, Color maximizing_player, int alpha, int 
                 std::cerr << "State of bitboards:" << std::endl;
                 exit(EXIT_FAILURE);
             }
-            int score = -quiescence(q_depth + 1, _game.get_current_turn(), -beta, -alpha, search_flag);
+            int score = -quiescence(q_depth + 1, maximizing_player, -beta, -alpha, search_flag);
             _game.unmake_move();
             if (score >= beta) {
                 return beta;
